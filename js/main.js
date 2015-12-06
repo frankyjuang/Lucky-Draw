123cafe7bf32331e4401e5affd52da689074c908
Yi-Lin Juang <frankyjuang@gmail.com>
2015-12-06 10:30:45 +0800

    [Final Version]

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 "use strict";
 let pic_list = [];
 let prize_idx = 0;
-let prize_list = ["耳罩耳機",
-                  "2KSound行動電源",
-                  "2KSound隨身音箱",
-                  "Adonit script 1/2",
-                  "Adonit script 2/2",
-                  "Adonit jot",
-                  "1028聖誕彩妝 1/5",
-                  "1028聖誕彩妝 2/5",
-                  "1028聖誕彩妝 3/5",
-                  "1028聖誕彩妝 4/5",
-                  "1028聖誕彩妝 5/5",
-                  "too cool for school 1/5",
-                  "too cool for school 2/5",
-                  "too cool for school 3/5",
-                  "too cool for school 4/5",
-                  "too cool for school 5/5",
-                  "Neogence 2"];
+let prize_list = [
+	"[ALTEAM 耳罩式耳機]",
+	"[2KSOUND 行動電源]",
+	"[2KSOUND 入耳式耳機]",
+	"[adonit SCRIPT觸控筆]",
+	"[adonit JOT TOUCH]",
+	"[1028 聖誕彩妝組合]",
+	"[too cool for school 化妝品組]",
+	"[Neogence 美容療程]",
+	"[Neogence 瓶裝玻尿酸保濕]"
+];
+
 $(function() {
+	$('#prizeTitle').text(prize_list[0]); 
     $('#fileSelector').change(function(evt) {
         let files = evt.target.files;
         for (let i = 0; i < files.length; i++) {
@@ -31,45 +27,51 @@ $(function() {
         console.log(pic_list);
         if (pic_list.length != 0) {
             let start = Math.floor(Math.random() * pic_list.length);
-            let steps = Math.floor(Math.random() * 20) + 20;
+            let steps = Math.floor(Math.random() * 20) + 30;
+            $('#prizeTitle').text(prize_list[prize_idx]);
             rollPics(start, steps);
         } else {
             console.error("No Pictures Left!");
-            $('#chosenPic').attr("src", "img/question.png");
+            $('#chosenPic').attr('src', "img/question.png");
         }
     });
     $('#nextBtn').click(function() {
+		$('#picPanel').css('background-color', "rgba(255, 255, 255, 0.5)");
         if (prize_idx + 1 == prize_list.length) {
             $('#prizeTitle').text("獎品抽完了QQQQ");
         } else {
             prize_idx++;
             $('#prizeTitle').text(prize_list[prize_idx]);
         }
-        $('#chosenPic').attr("src", "img/question.png");
+        $('#chosenPic').attr('src', "img/question.png");
     });
 });
 
 function rollPics(last, step) {
     let chosen;
+	$('#picPanel').css('background-color', "rgba(0, 0, 0, 0.5)");
     if (last + 1 == pic_list.length) {
         chosen = 0;
     } else {
         chosen = last + 1;
     }
     console.log(step);
-    $('#chosenPic').attr("src", "pic/" + pic_list[chosen]);
-    if (step > 20) {
+    $('#chosenPic').attr('src', "pic/" + pic_list[chosen]);
+    if (step > 25) {
         step--;
         setTimeout(function() {
             rollPics(chosen, step);
-        }, 200);
+        }, 100);
     } else if (step > 0) {
         step--;
-        console.log(3200 / Math.pow(2, step / 4));
         setTimeout(function() {
             rollPics(chosen, step);
-        }, 3200 / Math.pow(2, (step - 1) / 4));
+        }, 800 / Math.pow(2, (step - 1) / 4));
     } else {
+		setTimeout(function() {
+			$('#picPanel').css('background-color', "rgba(200, 200, 0, 0.5)");
+			$('#prizeTitle').text($('#prizeTitle').text() + "   恭喜你們!!!");
+        }, 1000);
         pic_list.splice(chosen, 1);
     }
 }
